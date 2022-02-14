268f548336266aa20ebb3b8e5fa8060048152a53
CarlMenger <56433847+CarlMenger@users.noreply.github.com>
2022-02-14 17:37:10 +0100

    Add files via upload

--- codeVsZombies/codeVsZombies.py
+++ codeVsZombies/codeVsZombies.py
@@ -15,16 +15,14 @@ print("", file=sys.stderr, flush=True)
 
 
 class Character:
-    _slots_ = ('id', 'x', 'y', 'coords')
+    _slots_ = ('id', 'x', 'y', 'points')
 
     def __init__(self, id: int, x: int, y: int) -> None:
         self.id: int = id
-        self.x: int = x  # TODO: use just point system and remove this
-        self.y: int = y  # TODO: use just point system and remove this
-        self.coords: tuple = (x, y)
+        self.point: tuple = (x, y)
 
     def __str__(self) -> str:
-        return f'I am f{self.id} at: {self.x}, {self.y}'
+        return f'I am f{self.id} at: {self.point[0]}, {self.point[1]}'
 
 
 class Human(Character):
@@ -36,39 +34,52 @@ class Zombie(Character):
     _slots_ = (
         'x_next',
         'y_next',
-        'coords_next',
-        'target',
-        'target_interception',
-        'target_coords',
+        'points_next',
+        'target_id',
+        'target_point',
         'target_distance',
-        '',
+        'interception_turns',
+        # 'interception_point',
+        # 'player_interception',
     )
 
     def __init__(self, id: int, x: int, y: int, x_next: int, y_next: int):
         super().__init__(id, x, y)
-        self.x_next: int = x_next  # TODO: use just point system and remove this
-        self.y_next: int = y_next  # TODO: use just point system and remove this
-        self.coords_next: tuple = (x_next, y_next)
-
-        self.target: int = -1
-        self.target_interception: float = -1
-        self.target_coords: tuple = get_target_coords()
+        self.point_next: tuple = (x_next, y_next)
+        self.target_id: int = -1
+        self.target_point: tuple = ()
         self.target_distance: float = float('inf')
-        self.interception: tuple = tuple()
+        self.interception_turns: float = -1
+        # self.set_target_vars()
 
-        self.player_interception = -1
-
-    def set_target_vars(self, humans):  # TODO: add new attrs
+    def set_target_vars(self):  # TODO: add new attrs
+        global HUMANS
         target_distance = float('inf')
-        for h in humans:
-            if h.distance <= target_distance:
-                self.target = h.id
-                target_distance = distance(h.coords, self.coords)
-        self.target_interception = target_distance // 400
+        # find closest target # TODO: not working
+        for h in HUMANS:
+            human_distance = distance(h.point, self.point)
+            if human_distance < target_distance:  # find closer target
+                self.target_id = h.id
+                target_distance = human_distance
 
-    # after each round, recalculate all distances/coords/interceptions
-    def recalculate_coors(self):
-        pass
+        # calc all other attrs
+        self.point_next = self.get_point_next()
+        self.target_point = h.point # HUMANS[self.target_id].point
+        self.target_distance = target_distance
+        print('self.target_distance: ', self.target_distance, file=sys.stderr)
+        self.interception_turns = (target_distance // 400) + 1
+        # self.interception_point =
+        # self.player_interception =
+
+    def get_point_next(self):
+        x0, y0 = self.point
+        x1, y1 = self.target_point
+        gradient = (y1 - y0) / (x1 - x0)
+        point0 = x0 + 400 / math.sqrt((1 + gradient ** 2))
+        point1 = x0 - 400 / math.sqrt((1 + gradient ** 2))
+        assert point0 is float and point1 is float, 'Wrong format in get_point_next() method'
+        return (point0, point1)
+
 
 def init_round_inputs():
     """ keep 2 lists of objects, keep player as last id/position human"""
@@ -98,35 +109,84 @@ def go_to(_from: tuple,
     return tuple([int((a + x) / 2), int((b + y) / 2)])  # FIXME this cant be proper tuple declaration
 
 
-def get_human(id: int, humans) -> Human:
-    out = [h for h in humans if h.id == id]
-    assert out, 'Human with f{id} not found!'
+def get_human(id: int) -> Human:
+    global HUMANS
+    out = [h for h in HUMANS if h.id == id]
+    print("out", out, file=sys.stderr, flush=True)
+    assert len(out) > 0, f'Human with ID: {id} not found!'
     return out[0]
 
 
-def go_to_human(id: int, humans: list):
-    player = get_human(len(humans), humans)
-    human = get_human(id, humans)
-    return go_to((player.x, player.y), (human.x, human.y))
+def go_to_human(id: int):
+    player = get_human(len(HUMANS) - 1)
+    return go_to(player.point, HUMANS[id].point)
 
 
-def find_mandatory_coords(humans):
-    return [z.target for z in zombies if z.target_interception <= 1.0]
+def recalculate_zombies() -> None:
+    global ZOMBIES
+    for z in ZOMBIES: z.set_target_vars()
+
+
+def get_t0_points():
+    return [z.point_next for z in ZOMBIES if z.interception_turns <= 1.0]
+
+
+def get_t1_points():
+    return [z.point_next for z in ZOMBIES if 1.0 <= z.interception_turns <= 2.0]
+
+
+# TODO, sorted list of inception turns and use that for point selection
+
+def get_circles_intersection(point_a, point_b, range_0, range_1):
+    # circle 1: (x0, y0), radius r0
+    # circle 2: (x1, y1), radius r1
+    print('gci input points:', point_a, point_b, file=sys.stderr)
+
+    distance = math.dist(point_a, point_b)
+    x0, y0 = point_a
+    x1, y1 = point_b
+    if distance > range_0 + range_1:  # no intersection
+        return None
+    elif distance > range_0 + range_1:  # 1 intersection
+        pass
+    # One circle within other
+    elif distance < abs(range_0 - range_1):
+        return None
+    # coincident circles
+    if distance == 0 and range_0 == range_1:
+        return None
+    else:  # 2 intersections
+        a = (range_0 ** 2 - range_1 ** 2 + distance ** 2) / (2 * distance)
+        h = math.sqrt(range_0 ** 2 - a ** 2)
+        x2 = x0 + a * (x1 - x0) / distance
+        y2 = y0 + a * (y1 - y0) / distance
+        x3 = x2 + h * (y1 - y0) / distance
+        y3 = y2 - h * (x1 - x0) / distance
+        x4 = x2 - h * (y1 - y0) / distance
+        y4 = y2 + h * (x1 - x0) / distance
+
+        return (x3, y3), (x4, y4)
 
 
 # game loop
 while True:
-    humans, zombies = init_round_inputs()
-    # print("humans after init",humans[0].id, file=sys.stderr, flush=True)
-    # print(humans)
-    mandatory_coords = find_mandatory_coords(humans)
-    if len(humans) == 1:
-        order = go_to_human(humans[0].id, humans)
-
+    HUMANS, ZOMBIES = init_round_inputs()
+    for z in ZOMBIES: z.set_target_vars()
     # default
+    # order = go_to_human(0)
+    # print("humans after init",humans[0].id, file=sys.stderr, flush=True)
+    t0_points = get_t0_points()
+    t1_points = get_t1_points()
+    print('t0_points: ', t0_points, file=sys.stderr)
+    print('t0_points: ', t1_points, file=sys.stderr)
+    print(ZOMBIES[0].interception_turns, file=sys.stderr)
+    if len(ZOMBIES) == 2:  # test scenario
+        a = ZOMBIES[0].point_next
+        b = ZOMBIES[1].point_next
+        intersection_1, intersection_2 = get_circles_intersection(a, b, 2000, 2000)
+        order = intersection_1
+    if len(HUMANS) == 1:
+        order = go_to_human(HUMANS[0].id)
 
-    order = go_to_human(0, humans)
-    # print(f'{order}')
+
     print(*order)
-
-
