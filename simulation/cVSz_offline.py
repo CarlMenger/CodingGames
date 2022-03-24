f0934c0b5dcf13260f1c1d5cfab9105d8b97e581
CarlMenger <56433847+CarlMenger@users.noreply.github.com>
2022-03-24 22:27:59 +0100

    Delete simulation directory

--- simulation/cVSz_offline.py
+++
@@ -1,9 +0,0 @@
-# from cVSz_classes import *
-from cVSz_funcs import *
-import timeit
-
-# print('TIME*load_offline_data: ', timeit.timeit(stmt=load_offline_data, number=1000)/1000)
-# print('TIME*calc_next_turn: ', timeit.timeit(stmt=calc_next_turn, number=1000)/1000)
-humans, zombies = load_offline_data()
-print(zombies)
-print(humans[0].point)
